c7159c0e4746602f436cc2c8d523593b267b54f5
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-06-12 03:01:36 +0300

    [C] change int to size_t, documentation

--- include/MergeSortLib/MergeSort.hpp
+++ include/MergeSortLib/MergeSort.hpp
@@ -1,10 +1,17 @@
 #pragma once
 #include <algorithm>
 
+/// Helper function that merges 2 adjacent sub-arrays on place.
+/// \tparam T
+/// \param arr - pointer to array being sorted in the main function
+/// \param temp - pointer to an axillary array
+/// \param from - an index of main array on which first sub-array starts
+/// \param mid - an index of main array on which first sub-array ends and second starts
+/// \param to - an index of main array on which second sub-array ends
 template <typename T>
-void merge(T* arr, T* temp, int from, int mid, int to)
+void merge(T* arr, T* temp, size_t from, size_t mid, size_t to)
 {
-    int temp_idx = 0, first_idx = from, second_idx = mid+1;
+    size_t temp_idx = 0, first_idx = from, second_idx = mid+1;
 
     while(first_idx <= mid && second_idx <= to) {
 
@@ -24,22 +31,29 @@ void merge(T* arr, T* temp, int from, int mid, int to)
         temp[temp_idx++] = arr[second_idx++];
     }
 
-    for (int arr_idx = from, temp_idx = 0; arr_idx <= to; arr_idx++, temp_idx++) {
+    for (size_t arr_idx = from, temp_idx = 0; arr_idx <= to; arr_idx++, temp_idx++) {
         arr[arr_idx] = temp[temp_idx];
     }
 }
 
+/// Main sorting function.
+/// \tparam T
+/// \param arr - pointer to array being sorted
+/// \param temp - pointer to an axillary array
+/// \param n - size of arr and temp
 template <typename T>
-void mergesort(T* arr, T* temp, int n)
+void mergesort(T* arr, T* temp, size_t n)
 {
-    for (int curr_size = 1; curr_size <= n-1; curr_size *= 2)
+    for (size_t curr_size = 1; curr_size <= n-1; curr_size *= 2)
     {
-        for (int left_start = 0; left_start < n-1; left_start += 2*curr_size)
+        for (size_t left_start = 0; left_start < n-1; left_start += 2*curr_size)
         {
-            int from = left_start;
-            int mid = std::min(left_start + curr_size - 1, n-1);
-            int to = std::min(left_start + 2*curr_size - 1, n-1);
+            // division of sorting part
+            size_t from = left_start;
+            size_t mid = std::min(left_start + curr_size - 1, n-1);
+            size_t to = std::min(left_start + 2*curr_size - 1, n-1);
 
+            // merging sorting parts
             merge(arr, temp, from, mid, to);
         }
     }
--- include/MergeSortLib/ParallelMergeSort.hpp
+++ include/MergeSortLib/ParallelMergeSort.hpp
@@ -1,13 +1,18 @@
 #include <thread>
 
+/// Helper function that merges 2 adjacent sub-arrays on place.
+/// \tparam T
+/// \param arr - pointer to array being sorted in the main function
+/// \param temp - pointer to an axillary array
+/// \param size - size of the sorting part
 template<typename T>
-void merge(T* arr, T* temp, int size)
+void merge(T* arr, T* temp, size_t size)
 {
-    int middle = size / 2;
+    size_t middle = size / 2;
 
-    int index_left = 0;
-    int index_right = middle;
-    int index_sequence = 0;
+    size_t index_left = 0;
+    size_t index_right = middle;
+    size_t index_sequence = 0;
 
     while (index_left < middle && index_right < size)
     {
@@ -23,18 +28,25 @@ void merge(T* arr, T* temp, int size)
     while (index_right < size)
         temp[index_sequence++] = arr[index_right++];
 
-    for (int i = 0; i < size; i++)
+    for (size_t i = 0; i < size; i++)
         arr[i] = temp[i];
 }
 
+/// Main sorting function.
+/// \tparam T
+/// \tparam min_size_to_thread - minimal amount of elements sorted in which another thread will be created
+/// \param arr - pointer to array being sorted
+/// \param temp - pointer to an axillary array
+/// \param size - size of arr and temp
 template<typename T, int min_size_to_thread = 10000>
-void parallel_mergesort(T* arr, T* temp, int size)
+void parallel_mergesort(T* arr, T* temp, size_t size)
 {
     if (size > 1)
     {
-        int middle = size / 2;
+        size_t middle = size / 2;
         if (size > min_size_to_thread)
         {
+            // creating threads for right and left part
             std::thread left(parallel_mergesort<T, min_size_to_thread>, &arr[0], temp, middle);
             std::thread right(parallel_mergesort<T, min_size_to_thread>, &arr[middle], temp, size - middle);
             left.join();
@@ -42,6 +54,7 @@ void parallel_mergesort(T* arr, T* temp, int size)
         }
         else
         {
+            // merge sort runs itself recurrently
             parallel_mergesort<T, min_size_to_thread>(&arr[0], temp, middle);
             parallel_mergesort<T, min_size_to_thread>(&arr[middle], temp, size - middle);
         }
