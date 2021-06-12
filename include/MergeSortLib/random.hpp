58c74e564ebc0e0186adb7395f664a119a0ae5be
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-06-12 03:01:50 +0300

    [A] add rendom documentation

--- include/MergeSortLib/random.hpp
+++ include/MergeSortLib/random.hpp
@@ -4,10 +4,16 @@
 #include <ctime>
 #include <concepts>
 
+// using mersenne twister
 std::random_device device;
 std::seed_seq seq{device(),(unsigned)std::time(nullptr)};
 std::mt19937 generator(seq);
 
+///
+/// \tparam T - integer type
+/// \param min - random number lower bound
+/// \param max - random number upper bound
+/// \return T - random number in bounds [min, max]
 template <typename T>
 T randomNum(T min, T max)
 {
